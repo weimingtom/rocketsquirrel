63cc00398b9bdedc9a62803fd0add50ecafd2e51
luisjimen@gmail.com <luisjimen@gmail.com@bee81afa-694e-ef19-0a5d-539bb85a3ddb>
2012-11-18 18:11:44 +0000

    Sample framework taken from libRocket samples Builded and copiled on Win32 and linux

--- trunk/sample/sample.cpp
+++ trunk/sample/sample.cpp
@@ -1,18 +1,81 @@
 #include <iostream>
 #include <string>
 #include "RocketSquirrel.h"
-#include <SFML/Window.hpp>
+
+#include "Shell.h"
+#include "Input.h"
+#include "ShellRenderInterfaceOpenGL.h"
+#include "ShellSystemInterface.h"
+
+#include "Rocket/Core.h"
+#include "Rocket/Debugger.h"
+
+
+static Rocket::Core::Context* context = NULL;
+
+
+void GameLoop()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	context->Update();
+	context->Render();
+
+	Shell::FlipBuffers();
+}
+
 
 int main()
 {
-	sf::Window MyWindow(sf::VideoMode(800, 600), "RocketSquirrel (Squirrel Script for libRocket) Sample");
+	// Generic OS initialisation
+	if (!Shell::Initialise("./") || !Shell::OpenWindow("RocketSquirrel Sample", true))
+	{
+		Shell::Shutdown();
+		return -1;
+	}
 
 
-	Rocket::Squirrel::Core::initialize();
+	// Rocket initialisation.
+	ShellRenderInterfaceOpenGL opengl_renderer;
+	Rocket::Core::SetRenderInterface(&opengl_renderer);
 
-	std::string strin;
+	// Initialise our system interface
+	ShellSystemInterface system_interface;
+	Rocket::Core::SetSystemInterface(&system_interface);
 
-	std::cin >> strin;
+	Rocket::Core::Initialise();
+
+	// Create the main Rocket context and set it on the shell's input layer.
+	context = Rocket::Core::CreateContext("main", Rocket::Core::Vector2i(1024, 768));
+	if (context == NULL)
+	{
+		Rocket::Core::Shutdown();
+		Shell::Shutdown();
+		return -1;
+	}
+
+	Rocket::Debugger::Initialise(context);
+	Input::SetContext(context);
+
+	Shell::LoadFonts("./assets/");
+
+	// Load and show the demo document.
+	Rocket::Core::ElementDocument* document = context->LoadDocument("./assets/demo.rml");
+	if (document != NULL)
+	{
+		document->Show();
+		document->RemoveReference();
+	}
+
+	//Main Loop
+	Shell::EventLoop(GameLoop);
+
+	// Shutdown Rocket.
+	context->RemoveReference();
+	Rocket::Core::Shutdown();
+
+	Shell::CloseWindow();
+	Shell::Shutdown();
 
 	return 0;
 }
